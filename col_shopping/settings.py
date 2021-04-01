029c879bf61084ae43ce4ebfa16464ad16c1ea3a
Priyanka7011 <pk3201134@gmail.com>
2021-04-01 19:54:03 +0530

    added new apps

--- col_shopping/settings.py
+++ col_shopping/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chatApp',
-    'clientRoom'
+    'clientRoom',
+    'SharedCart'
 ]
 
 MIDDLEWARE = [
